3664bd2dedf627919c91d716dd0daf3f89a39591
Aran Nolan <nolanar@tcd.ie>
2015-10-14 17:24:39 +0100

    Trailing null characters in StringContent removed.
    The string created from the packet byte array was being padded with null characters to a length of 2^16 (PACKETSIZE). This was causing a graphical glitch when printed to the server terminal.

--- Assign1/src/cs/tcd/ie/PacketContent.java
+++ Assign1/src/cs/tcd/ie/PacketContent.java
@@ -3,6 +3,6 @@ package cs.tcd.ie;
 import java.net.DatagramPacket;
 
 public interface PacketContent {
-	public String toString();
-	public DatagramPacket toDatagramPacket();
+    public String toString();
+    public DatagramPacket toDatagramPacket();
 }
--- Assign1/src/cs/tcd/ie/Server.java
+++ Assign1/src/cs/tcd/ie/Server.java
@@ -6,53 +6,53 @@ import java.net.DatagramSocket;
 import tcdIO.Terminal;
 
 public class Server extends Node {
-	static final int DEFAULT_PORT = 50001;
+    static final int DEFAULT_PORT = 50001;
 
-	Terminal terminal;
-	
-	/*
-	 * 
-	 */
-	Server(Terminal terminal, int port) {
-		try {
-			this.terminal= terminal;
-			socket= new DatagramSocket(port);
-			listener.go();
-		}
-		catch(java.lang.Exception e) {e.printStackTrace();}
-	}
+    Terminal terminal;
 
-	/**
-	 * Assume that incoming packets contain a String and print the string.
-	 */
-	public void onReceipt(DatagramPacket packet) {
-		try {
-			StringContent content= new StringContent(packet);
+    /*
+     * 
+     */
+    Server(Terminal terminal, int port) {
+        try {
+            this.terminal= terminal;
+            socket= new DatagramSocket(port);
+            listener.go();
+        }
+        catch(java.lang.Exception e) {e.printStackTrace();}
+    }
 
-			terminal.println(content.toString());
+    /**
+     * Assume that incoming packets contain a String and print the string.
+     */
+    public void onReceipt(DatagramPacket packet) {
+        try {
+            StringContent content= new StringContent(packet);
 
-			DatagramPacket response;
-			response= (new StringContent("OK")).toDatagramPacket();
-			response.setSocketAddress(packet.getSocketAddress());
-			socket.send(response);
-		}
-		catch(Exception e) {e.printStackTrace();}
-	}
+            terminal.println(content.toString());
 
-	
-	public synchronized void start() throws Exception {
-		terminal.println("Waiting for contact");
-		this.wait();
-	}
-	
-	/*
-	 * 
-	 */
-	public static void main(String[] args) {
-		try {					
-			Terminal terminal= new Terminal("Server");
-			(new Server(terminal, DEFAULT_PORT)).start();
-			terminal.println("Program completed");
-		} catch(java.lang.Exception e) {e.printStackTrace();}
-	}
+            DatagramPacket response;
+            response = (new StringContent("OK")).toDatagramPacket();
+            response.setSocketAddress(packet.getSocketAddress());
+            socket.send(response);
+        }
+        catch(Exception e) {e.printStackTrace();}
+    }
+
+
+    public synchronized void start() throws Exception {
+        terminal.println("Waiting for contact");
+        this.wait();
+    }
+
+    /*
+     * 
+     */
+    public static void main(String[] args) {
+        try {					
+            Terminal terminal= new Terminal("Server");
+            (new Server(terminal, DEFAULT_PORT)).start();
+            terminal.println("Program completed");
+        } catch(java.lang.Exception e) {e.printStackTrace();}
+    }
 }

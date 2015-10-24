c3f8307cd322922bd2988954bc8da7792ccb25c3
Aran Nolan <nolanar@tcd.ie>
2015-10-24 13:30:06 +0100

    Use new sample code as starting point
    More advanced sample code has been provided as a starting point. It transfers a file rather than just a text message.

--- assignment1/StopAndWait/src/cs/tcd/ie/Server.java
+++ assignment1/StopAndWait/src/cs/tcd/ie/Server.java
@@ -6,57 +6,58 @@ import java.net.DatagramSocket;
 import tcdIO.Terminal;
 
 public class Server extends Node {
-    static final int DEFAULT_PORT = 50001;
+	static final int DEFAULT_PORT = 50001;
 
-    Terminal terminal;
+	Terminal terminal;
+	
+	/*
+	 * 
+	 */
+	Server(Terminal terminal, int port) {
+		try {
+			this.terminal= terminal;
+			socket= new DatagramSocket(port);
+			listener.go();
+		}
+		catch(java.lang.Exception e) {e.printStackTrace();}
+	}
 
-    /*
-     * 
-     */
-    Server(Terminal terminal, int port) {
-        try {
-            this.terminal= terminal;
-            socket= new DatagramSocket(port);
-            listener.go();
-        }
-        catch(java.lang.Exception e) {e.printStackTrace();}
-    }
+	/**
+	 * Assume that incoming packets contain a String and print the string.
+	 */
+	public void onReceipt(DatagramPacket packet) {
+		try {
+			terminal.println("Received packet");
+			
+			PacketContent content= PacketContent.fromDatagramPacket(packet);
 
-    /**
-     * Assume that incoming packets contain a String and print the string.
-     */
-    public void onReceipt(DatagramPacket packet) {
-        try {
-            StringContent content= new StringContent(packet);
+			if (content.getType()==PacketContent.FILEINFO) {
+				terminal.println("File name: " + ((FileInfoContent)content).getFileName());
+				terminal.println("File size: " + ((FileInfoContent)content).getFileSize());
 
-            terminal.println(content.toString());
-            
-            synchronized(this) {
-                this.wait(5000);        //simulated latency
-            }
-            
-            DatagramPacket response;
-            response = (new StringContent("OK")).toDatagramPacket();
-            response.setSocketAddress(packet.getSocketAddress());
-            socket.send(response);
-        }
-        catch(Exception e) {e.printStackTrace();}
-    }
+				DatagramPacket response;
+				response= new AckPacketContent("OK - Received this").toDatagramPacket();
+				response.setSocketAddress(packet.getSocketAddress());
+				socket.send(response);
+			}
+		}
+		catch(Exception e) {e.printStackTrace();}
+	}
 
-
-    public synchronized void start() throws Exception {
-        terminal.println("Waiting for contact");
-        this.wait();
-    }
-
-    /*
-     * 
-     */
-    public static void main(String[] args) {
-        try {					
-            Terminal terminal= new Terminal("Server");
-            (new Server(terminal, DEFAULT_PORT)).start();
-            terminal.println("Program completed");
-        } catch(java.lang.Exception e) {e.printStackTrace();}
-    }
+	
+	public synchronized void start() throws Exception {
+		terminal.println("Waiting for contact");
+		this.wait();
+	}
+	
+	/*
+	 * 
+	 */
+	public static void main(String[] args) {
+		try {					
+			Terminal terminal= new Terminal("Server");
+			(new Server(terminal, DEFAULT_PORT)).start();
+			terminal.println("Program completed");
+		} catch(java.lang.Exception e) {e.printStackTrace();}
+	}
 }

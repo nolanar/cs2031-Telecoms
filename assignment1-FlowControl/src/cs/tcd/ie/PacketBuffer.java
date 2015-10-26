bcf9b6a67289a90970a4c3ed8e30cbeb8eba4d8f
Aran Nolan <nolanar@tcd.ie>
2015-10-26 17:26:17 +0000

    Add worker classes to PacketBuffer
    Feeder feeds packets from buffer to window and schedule.
    ScheduleHandler handles packets when delay expires on them.

--- assignment1-FlowControl/src/cs/tcd/ie/PacketBuffer.java
+++ assignment1-FlowControl/src/cs/tcd/ie/PacketBuffer.java
@@ -10,12 +10,70 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class PacketBuffer {
     
     private LinkedBlockingQueue<PacketContent> buffer;
-    private Window window;
+    private CapacityBlockingQueue<ScheduledPacket> window;
+    private DelayQueue<ScheduledPacket> schedule;
+
+    private final int sequenceLength;
+    private int bufferNumber;
     
-    private class Window {
+    public PacketBuffer(int windowSize, int sequenceLength) {
+        buffer = new LinkedBlockingQueue<>();
+        window = new CapacityBlockingQueue<>(windowSize);
+        schedule = new DelayQueue<>();
         
-        CapacityBlockingQueue<ScheduledPacket> window;
-        DelayQueue<ScheduledPacket> scheduler;
-            
+        this.sequenceLength = sequenceLength;
+        bufferNumber = 0;
     }
+    
+    public boolean add(PacketContent packet) {
+        packet.number = bufferNumber;
+        bufferNumber = (bufferNumber + 1) % sequenceLength;
+        return buffer.add(packet);
+    }
+    
+    /**
+     * Feeds packets from buffer to window.
+     * 
+     * Packets are fed from buffer to window when buffer is not empty and
+     * window is not full. Any packets added to window are also added to
+     * schedule.
+     */
+    private class Feeder implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    window.awaitNotFull();                // Blocking
+                    PacketContent packet = buffer.take(); // Blocking
+                    ScheduledPacket schPacket = new ScheduledPacket(packet);
+                    window.add(schPacket);
+                    schedule.add(schPacket);
+
+                } catch (InterruptedException e) {
+                    System.out.println("Terminating Feeder"); //--> DEBUG
+                    return;
+                }
+            }
+        }
+
+    }
+    
+    private class ScheduleHandler implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    ScheduledPacket schPacket = schedule.take(); // Blocking
+                    schedule.put(schPacket.repeat());
+                    //send(schPacket);                           //<--IMPLEMENT
+                } catch (InterruptedException e) {
+                    System.out.println("Terminating ScheduleHandler");
+                    return;
+                }
+            }            
+        }
+    }
+    
 }

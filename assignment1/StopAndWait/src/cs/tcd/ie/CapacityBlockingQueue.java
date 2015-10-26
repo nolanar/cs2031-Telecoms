35fca7394708868f82249a08969a6a5acd0c9c00
Aran Nolan <nolanar@tcd.ie>
2015-10-26 15:36:43 +0000

    Refactor CapacityBlockingQueue

--- assignment1/StopAndWait/src/cs/tcd/ie/CapacityBlockingQueue.java
+++ assignment1/StopAndWait/src/cs/tcd/ie/CapacityBlockingQueue.java
@@ -1,43 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs.tcd.ie;
 
+/**
+ *
+ * @author aran
+ */
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.concurrent.locks.*;
 
 /**
  *
- * @author aran
  * @param <E> Type of queue elements
  */
 public class CapacityBlockingQueue<E> {
 
-    final Lock lock;
-    final Condition notFull;
+    private final Lock lock;
+    private final Condition notFull;
 
-    final Queue<E> buffer;
-    final int capacity;
+    private final Queue<E> content;
+    private final int capacity;
 
     public CapacityBlockingQueue(int capacity) {
         lock = new ReentrantLock();
         notFull = lock.newCondition();
 
-        buffer = new LinkedList<>(); // implemented with LinkedList
+        content = new LinkedList<>(); // implemented with LinkedList
         this.capacity = capacity;
     }
 
     public boolean add(E e) {
-        return buffer.add(e);
+        return content.add(e);
     }
 
     public E take() {
         lock.lock();
         try {
-            E e = buffer.remove();
+            E e = content.remove();
             if (!isFull()) {
                 notFull.signalAll();
             }
@@ -64,6 +62,6 @@ public class CapacityBlockingQueue<E> {
     }
 
     public boolean isFull() {
-        return buffer.size() >= capacity;
+        return content.size() >= capacity;
     }
 }

274b4f8c1f7abfe85d8debbfe8cffc4f054736b2
Aran Nolan <nolanar@tcd.ie>
2015-10-27 03:29:23 +0000

    Replace CapacityBlockingQueue with ArrayBlockingList
    Added list like functionality. 
    Backed by an ArrayList instead of a Queue.
    set() and get() methods added.

---
+++ assignment1-FlowControl/src/cs/tcd/ie/ArrayBlockingList.java
@@ -0,0 +1,244 @@
+package cs.tcd.ie;
+
+/**
+ *
+ * @author aran
+ */
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * A data structure with functionality of both a fixed sized list and a queue.
+ * 
+ * 
+ * 
+ * @param <E> - Type of list elements
+ */
+public class ArrayBlockingList<E> {
+
+    private final ReentrantLock lock;
+    private final Condition notFull;
+    private final Condition notEmpty;
+
+    private final ArrayList<E> items;
+    private final int capacity;
+    private int front;
+    private int back;
+    private int count;
+
+    public ArrayBlockingList(int capacity) {
+        lock = new ReentrantLock();
+        notFull = lock.newCondition();
+        notEmpty = lock.newCondition();
+
+        items = new ArrayList<E>(capacity);
+        this.capacity = capacity;
+    }
+
+    /**
+     * Inserts element at back of queue if possible. 
+     * 
+     * @param e - the element to add
+     * @return true if element added to list
+     */
+    public boolean add(E e) {
+        lock.lock();
+        try {
+            if (isFull()) {
+                throw new IllegalStateException();
+            }
+            enqueue(e);
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    /**
+     * Inserts the specified element at the tail of this queue.
+     * 
+     * It will block the calling thread until space becomes available
+     * if the list is full.
+     *
+     * @param e - the element to add
+     * @throws InterruptedException
+     */
+    public void put(E e) throws InterruptedException {
+        lock.lock();
+        try {
+            while (isFull()) {
+                notFull.await();
+            }
+            enqueue(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    /**
+     * Replaces the element at the specified position in this list with the specified element.
+     * 
+     * Will cause queue to expand if setting the back of the queue.
+     * 
+     * @param index - index of the element to replace
+     * @param e - element to be stored at the specified position
+     * @return the element previously at the specified position
+     */
+    public E set(int index, E e) {
+        lock.lock();
+        try {
+            if (index > 0 || index < capacity - 1) {
+                throw new IndexOutOfBoundsException();
+            }
+            int relative = (index - front + capacity) % capacity;
+            E result = items.get(relative);
+            if (index == back) {
+                enqueue(e);
+            } else {
+                items.set(relative, e);
+            }
+            return result;
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    /**
+     * Retrieves and removes the head of this queue.
+     * 
+     * @return the head of this list
+     */
+    public E remove() {
+        lock.lock();
+        try {
+            if (isEmpty()) {
+                throw new NoSuchElementException();
+            }
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Retrieves and removes the head of this queue.
+     * 
+     * It will block the calling thread until an element becomes available.
+     *
+     * @return the head of this list
+     * @throws InterruptedException
+     */
+    public E take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (isEmpty()) {
+                notFull.await();
+            }
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    /**
+     * Returns the element at the specified position in this list, or null
+     * if that position is empty.
+     * 
+     * @param index - position in list
+     * @return the element at the position, null if that position is empty.
+     */
+    public E get(int index) {
+        lock.lock();
+        try {
+            if (index > 0 || index < capacity - 1) {
+                throw new IndexOutOfBoundsException();
+            }
+            int relative = (index - front + capacity) % capacity;
+            return items.get(relative);
+        } finally {
+            lock.unlock();
+        }
+
+    }
+    
+    /**
+     * Blocks the calling thread until the queue has space.
+     * 
+     * @throws InterruptedException 
+     */
+    public void awaitNotFull() throws InterruptedException {
+        lock.lock();
+        try {
+            while (isFull()) {
+                notFull.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    
+    /**
+     * Returns true if queue is full, false otherwise.
+     * 
+     * Call only when holding lock
+     */
+    private boolean isFull() {
+        return count == capacity;
+    }
+
+    /**
+     * Returns true if queue is empty, false otherwise.
+     * 
+     * Call only when holding lock
+     */
+    private boolean isEmpty() {
+        return count == 0;
+    }
+    
+    /** 
+     * Cyclically increments number.
+     */
+    private int incr(int number) {
+        if (++number == capacity) {
+            number = 0;
+        }
+        return number;
+    }
+    
+    /**
+     * Inserts element into back of queue and signals notEmpty.
+     * 
+     * Any elements that become contiguous to the back of the queue become 
+     * part of the queue.
+     * 
+     * Call only when holding lock.
+     */
+    private void enqueue(E e) {
+        items.set(back, e);
+        E next;
+        do {
+            back = incr(back);
+            count++;
+            next = items.get(back);
+        } while (!isFull() && next != null);
+        notEmpty.signal();
+    }    
+
+    /**
+     * Extracts element at front of queue and signals notFull.
+     * 
+     * Call only when holding lock.
+     */
+    private E dequeue() {
+        E e = items.get(front);
+        items.set(front, null);
+        front = incr(front);
+        count--;
+        notFull.signal();
+        return e;
+    }  
+    
+}
